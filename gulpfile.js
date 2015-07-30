e860f5699d57b79143832b016144dd021576afc9
Connor Peet <connor@peet.io>
2015-07-30 18:45:44 +1000

    Init

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var glob = require('glob');
+var $ = require('gulp-load-plugins')();
+var minify = false;
+
+
+gulp.task('html', function () {
+    return gulp.src('src/*.jade')
+        .pipe($.jade())
+        .pipe($.if(minify, $.htmlmin()))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('css', ['html'], function () {
+    return gulp.src('src/css/style.less')
+        .pipe($.less())
+        .pipe($.autoprefixer())
+        .pipe($.if(minify, $.uncss({ html: glob.sync('dist/*.html') })))
+        .pipe($.if(minify, $.minifyCss()))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('js', function () {
+    return gulp.src('src/js/*.js')
+        .pipe($.if(minify, $.uglify()))
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('images', function () {
+    return gulp.src('src/img/**')
+        .pipe($.if(minify, $.imagemin()))
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('misc', function () {
+    return gulp.src([])
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('minify', function () {
+    minify = true;
+});
+
+gulp.task('default', [ 'html', 'css', 'js', 'images', 'misc' ]);
+gulp.task('minify', [ 'minify', 'default' ]);
